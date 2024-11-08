416d0a32d6b5d14b280215a5e6834b5ea6902586
Hee Seo Kim <hisounni@ewhain.net>
2024-11-08 22:45:51 +0900

    tst

---
+++ TigerDisaster/Assets/Scripts/prologue.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class prologue : MonoBehaviour
+{
+    public Text dialogueText;               // 대화창 텍스트
+
+    private string[] dialogues = {
+        "으으... 여기가 어디지. 분명 난 호랑이한테 잡혀갔는데...",
+        "얼른 산 아래로 내려가야겠다."
+    };
+
+    private int dialogueIndex = 0;
+    private bool isTyping = false;
+
+    private void Start()
+    {
+        DisplayNextDialogue();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isTyping)
+        {
+            DisplayNextDialogue();
+        }
+    }
+
+    private void DisplayNextDialogue()
+    {
+        if (dialogueIndex < dialogues.Length)
+        {
+            StartCoroutine(TypeDialogue(dialogues[dialogueIndex]));
+            dialogueIndex++;
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private IEnumerator TypeDialogue(string dialogue)
+    {
+        isTyping = true;
+        dialogueText.text = "";
+
+        foreach (char letter in dialogue.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.05f); // 글자 타이핑 속도 조절
+        }
+
+        isTyping = false;
+    }
+
+    private void EndDialogue()
+    {
+        
+        dialogueText.text = "";
+    }
+}
