fc0ed84fd62eab704c473a2ce79e7977fd930940
EPSIHYEON <127606365+EPSIHYEON@users.noreply.github.com>
2024-11-09 13:25:26 +0900

    사진추가

--- TigerDisaster/Assets/Scripts/Turn_Manager.cs
+++ TigerDisaster/Assets/Scripts/Turn_Manager.cs
@@ -5,17 +5,25 @@ using UnityEngine.UI;
 using System;
 
 
-public class Panel_Move : MonoBehaviour
+public class Turn_Manager : MonoBehaviour
 {
     public List<GameObject> panels;
     private List<int> conditions = new List<int> { 0, 1, 0, 1 };
+
+
     private int currentnum = 1;
     public Button button;
     public GameObject circularImageObject;
     public Image circularImage;
     public float duration = 1f;  // 슬라이더가 1에서 0으로 줄어드는 시간
     public GameObject[] Ghosts;
-    
+    private SaveManager saveMan;
+
+    void Start()
+    {
+        
+    }
+
 
 
     void Update()
