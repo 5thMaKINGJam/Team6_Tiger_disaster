aad27c02bbeccb90b50bdf3f22ab07fc497b6a99
Hee Seo Kim <hisounni@ewhain.net>
2024-11-10 02:34:38 +0900

    tst

--- TigerDisaster/Assets/Scripts/FadeController.cs
+++ TigerDisaster/Assets/Scripts/FadeController.cs
@@ -21,8 +21,17 @@ public class FadeController : MonoBehaviour
     public void FadeOut(string nextSceneName)
     {
         panel.SetActive(true);
-        RegisterCallback(() => LoadNextScene(nextSceneName));
+        RegisterCallback(() => StartCoroutine(LoadSceneWithFadeIn(nextSceneName)));
         StartCoroutine(CoFadeOut());
+    }
+
+    private IEnumerator LoadSceneWithFadeIn(string sceneName)
+    {
+        // 씬 로드 후 FadeIn을 진행합니다.
+        SceneManager.LoadScene(sceneName);
+        yield return null; // 한 프레임 대기하여 씬이 완전히 로드되도록 합니다.
+
+        StartCoroutine(CoFadeIn());
     }
 
     IEnumerator CoFadeIn()
@@ -57,10 +66,5 @@ public class FadeController : MonoBehaviour
     public void RegisterCallback(Action callback)
     {
         onCompleteCallback = callback;
-    }
-
-    private void LoadNextScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
     }
 }
