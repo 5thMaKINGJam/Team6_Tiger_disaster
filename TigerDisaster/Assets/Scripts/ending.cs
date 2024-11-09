e7a5259173b779c4f43ab986edd4eb9d753fe3fe
Hee Seo Kim <hisounni@ewhain.net>
2024-11-09 20:55:15 +0900

    tst

--- TigerDisaster/Assets/Scripts/ending.cs
+++ TigerDisaster/Assets/Scripts/ending.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class ending : MonoBehaviour
 {
+    private FadeController fadeController;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 페이드인&아웃
+        fadeController = FindObjectOfType<FadeController>();
+        if (fadeController != null)
+        {
+            fadeController.RegisterCallback(OnFadeOutComplete); // 페이드아웃 후 진행할 액션 등록
+            fadeController.FadeOut(); // FadeOut 호출
+        }
     }
 
     // Update is called once per frame
